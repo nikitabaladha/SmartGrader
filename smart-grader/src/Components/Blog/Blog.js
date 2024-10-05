a0cd5c8a8a07f545ef8357df029517fa77008ae8
nikitabaladha <nikitabaladha1996@gmail.com>
2024-10-05 16:27:29 +0530

    Basic html page done for blog post

--- smart-grader/src/Components/Blog/Blog.js
+++ smart-grader/src/Components/Blog/Blog.js
@@ -1,10 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import "./Blog.css";
+import posts from "./Posts";
+
+import { IoIosVideocam } from "react-icons/io";
+import { FaArrowRight } from "react-icons/fa6";
 
 const Blog = () => {
   return (
     <>
       <Navbar />
+      <div className="container-fluid">
+        <div className="container-fluid blog-page-first">
+          <h2>Our Blog</h2>
+          <p>
+            We use an agile approach to test assumptions and connect with the
+            needs of your audience early and often.
+          </p>
+        </div>
+
+        <div className="container-fluid blog-page-second">
+          <div className="row">
+            {posts.map((post, index) => (
+              <div className="col-md-6" key={index}>
+                <div className="card">
+                  <div className="card-body">
+                    <h6 className="card-subtitle">
+                      <IoIosVideocam /> Tutorial {post.date}
+                    </h6>
+                    <h5 className="card-title">{post.title}</h5>
+                    <p className="card-text">{post.description}</p>
+                    <img src={post.image} className="" alt={post.title} />
+                    <p>{post.author}</p>
+                    <Link href={post.link}>
+                      Read more <FaArrowRight />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
---
+++ smart-grader/src/Components/Blog/Posts.js
@@ -0,0 +1,22 @@
+const posts = [
+  {
+    date: "2024-06-18",
+    title: "Revolutionizing Interviews with AI-Powered Feedback",
+    description:
+      "In the fast-paced world of hiring, SmartGrader's AI-powered feedback system is revolutionizing how interviews are conducted. Discover how instant, AI-driven feedback can enhance your inter...",
+    author: "SmartGrader Team",
+    link: "/blog/revolutionizing-interviews",
+    image: "https://example.com/interview-image.jpg",
+  },
+  {
+    date: "2024-06-17",
+    title: "Enhancing Academic Integrity with SmartGrader",
+    description:
+      "Academic institutions face challenges in maintaining integrity during exams. Learn how SmartGrader's secure testing environment ensures fairness and integrity in academic assessments. With features ...",
+    author: "SmartGrader Team",
+    link: "/blog/enhancing-academic-integrity",
+    image: "https://example.com/academic-integrity-image.jpg",
+  },
+];
+
+export default posts;

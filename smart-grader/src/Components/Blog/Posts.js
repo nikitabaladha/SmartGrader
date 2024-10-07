c8b8110641bd03e27fd0ca33fe4575a005c8fe5a
nikitabaladha <nikitabaladha1996@gmail.com>
2024-10-08 01:44:31 +0530

    FullBlog.js page done with minor css and displaying whole artical

--- smart-grader/src/Components/Blog/Posts.js
+++ smart-grader/src/Components/Blog/Posts.js
@@ -1,9 +1,13 @@
+import demoBlogImage from "../../Images/demo-blog-1.jpg";
+
 const posts = [
   {
     date: "2024-06-18",
     title: "Want to ace your exams? Here’s the secret!",
+    image: demoBlogImage,
     description: `
-          <p>IIT Academy – “Let’s Crack It!”</p>
+        <section>
+          <h2>IIT Academy – “Let’s Crack It!”</h2>
           <blockquote>“Hard work beats talent when talent doesn’t work hard”</blockquote>
           <p>As a student, you know how crucial exams like JEE and NEET are for your future. They open doors to numerous opportunities. Have you ever wondered why some students do so well in these exams while others struggle? The answer lies in three key areas:</p>
           <ul>
@@ -11,59 +15,143 @@ const posts = [
             <li>The right mindset</li>
             <li>And a few simple tips.</li>
           </ul>
-          <h3>Start Early: Don’t Wait for the Last Minute</h3>
+        </section>
+        <section>
+          <h2>Start Early: Don’t Wait for the Last Minute</h2>
           <p>Starting your preparation early is essential. It keeps you ahead, builds your confidence, and reduces stress. Imagine having an entire year to learn and practice instead of just a few months. Begin early, and you’ll be amazed at how much you can achieve! Join IIT Academy for a great experience. Let’s crack it!</p>
-          <h3>Set Goals: Break Them Down into Smaller Targets</h3>
+        </section>
+        <section>
+          <h2>Set Goals: Break Them Down into Smaller Targets</h2>
           <p>Setting goals is vital, but breaking them down into smaller, achievable targets is equally important. This keeps you focused, motivated, and helps you track your progress. For example, if you want to master a subject, divide it into smaller topics and tackle each one step by step. At IIT Academy, our teachers are always here to guide you through this process.</p>
-          <h3>Practice Consistently: Build Confidence and Strength</h3>
+        </section>
+        <section>
+          <h2>Practice Consistently: Build Confidence and Strength</h2>
           <p>Regular practice is key to success. It builds confidence, helps you improve weak areas, and develops problem-solving skills. Try to practice a little every day. You’ll be surprised at how much you can improve!</p>
-          <h3>Analyze and Improve: Review and Refine Your Strategy</h3>
+        </section>
+        <section>
+          <h2>Analyze and Improve: Review and Refine Your Strategy</h2>
           <p>Reviewing your performance is crucial. It helps you identify areas for improvement, adjust your strategy, and stay on course. Don’t be afraid to ask for help, seek feedback, and learn from your mistakes.</p>
-          <h3>Stay Positive: Believe in Yourself and Stay Motivated</h3>
-          <p>Believing in yourself is half the battle! Stay positive, even when faced with challenges. Remind yourself of your strengths, celebrate your achievements, and keep moving forward.</p>
-          <p>Success in exams requires a mix of hard work, smart work, and the right mindset. By following these tips, you’ll be well on your way to excelling in your exams!</p>
-          <p>At IIT Academy, we are dedicated to helping students unlock their potential. Our expert faculty, comprehensive curriculum, and supportive environment are designed to help you succeed. Stay tuned for more tips, guidance, and resources to help you achieve your goals!</p>
+        </section>     
+        <section> 
+            <h2>Stay Positive: Believe in Yourself and Stay Motivated</h2>
+            <p>Believing in yourself is half the battle! Stay positive, even when faced with challenges. Remind yourself of your strengths, celebrate your achievements, and keep moving forward.</p>
+            <p>Success in exams requires a mix of hard work, smart work, and the right mindset. By following these tips, you’ll be well on your way to excelling in your exams!</p>
+            <p>At IIT Academy, we are dedicated to helping students unlock their potential. Our expert faculty, comprehensive curriculum, and supportive environment are designed to help you succeed. Stay tuned for more tips, guidance, and resources to help you achieve your goals!</p>
+        </section>
         `,
     link: "/blog/ace-your-exams",
   },
   {
-    date: "2024-06-19",
+    date: "2024-06-20",
     title: "Unlock the Secrets of JEE Mains and Advanced!",
+    image: demoBlogImage,
     description: `
-          <p>Are you dreaming of cracking the prestigious JEE exams? Want to know the difference between JEE Mains and Advanced? Let’s clear up any confusion and get you started on the path to success!</p>
-          <h3>JEE Mains:</h3>
-          <ul>
-            <li>For admission to NITs, IIITs, and GFTIs</li>
-            <li>Objective-type questions</li>
-            <li>3 hours long</li>
-            <li>300 marks</li>
-            <li>Eligibility: 75% in 12th standard (65% for SC/ST)</li>
-          </ul>
-          <h3>JEE Advanced:</h3>
-          <ul>
-            <li>For admission to IITs and ISM</li>
-            <li>Objective-type questions</li>
-            <li>2 papers (Paper 1 and Paper 2)</li>
-            <li>3 hours each</li>
-            <li>366 marks</li>
-            <li>Eligibility: Top 2.5 lakh rank holders in JEE Mains</li>
-          </ul>
-          <p>JEE Mains is the first step: It’s the first exam you need to pass to qualify for JEE Advanced. Doing well in JEE Mains is crucial.</p>
-          <p>JEE Advanced is tougher: This exam is more challenging and competitive than JEE Mains. Be ready for harder questions and a more intense experience.</p>
-          <h3>Tips and tricks to crack these exams:</h3>
-          <ul>
-            <li><strong>Practice regularly:</strong> Regular practice helps you gain confidence, improve weak areas, and develop problem-solving skills. Make it a habit to practice daily. IIT Academy teachers are here to help you.</li>
-            <li><strong>Focus on Basics:</strong> Clear your basics in each subject because they are the foundation for advanced concepts. IIT Academy offers regular doubt-clearing sessions.</li>
-            <li><strong>Time management:</strong> Learn to manage your time effectively during the exams. IIT Academy provides regular mock tests.</li>
-            <li><strong>Stay motivated and focused:</strong> Believe in yourself and stay positive. Celebrate small victories and keep pushing forward.</li>
-            <li><strong>Understand the exam pattern:</strong> Know the format, marking scheme, and types of questions to avoid surprises on exam day.</li>
-            <li><strong>Quality study materials:</strong> Use good study resources like textbooks, online materials, and coaching classes. IIT Academy offers excellent chapter-wise study materials.</li>
-            <li><strong>Seek help when needed:</strong> Don’t hesitate to ask teachers, mentors, or friends for help if you face difficulties.</li>
-            <li><strong>Stay healthy and focused:</strong> Take care of your physical and mental health by getting enough sleep and eating a balanced diet.</li>
-          </ul>
-          <p>By following these tips, you’ll be well-prepared to tackle JEE Mains and Advanced with confidence!</p>
-          <p>Stay tuned for more tips and guidance from IIT Academy!</p>
-        `,
+    <section>
+        <h2>Unlock the Secrets of JEE Mains and Advanced!</h2>
+        <p>
+          Are you dreaming of cracking the prestigious JEE exams? Want to know
+          the difference between JEE Mains and Advanced? Let’s clear up any
+          confusion and get you started on the path to success!
+        </p>
+    </section>
+
+    <section>
+        <h2>JEE Mains:</h2>
+        <ul>
+          <li>For admission to NITs, IIITs, and GFTIs</li>
+          <li>Objective-type questions</li>
+          <li>3 hours long</li>
+          <li>300 marks</li>
+          <li>Eligibility: 75% in 12th standard (65% for SC/ST)</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>JEE Advanced:</h2>
+        <ul>
+          <li>For admission to IITs and ISM</li>
+          <li>Objective-type questions</li>
+          <li>2 papers (Paper 1 and Paper 2)</li>
+          <li>3 hours each</li>
+          <li>366 marks</li>
+          <li>Eligibility: Top 2.5 lakh rank holders in JEE Mains</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>JEE Mains is the first step:</h2>
+        <p>
+          It’s the first exam you need to pass to qualify for JEE Advanced.
+          Doing well in JEE Mains is crucial.
+        </p>
+    </section>
+
+    <section>
+        <h2>JEE Advanced is tougher:</h2>
+        <p>
+          This exam is more challenging and competitive than JEE Mains. Be ready
+          for harder questions and a more intense experience.
+        </p>
+    </section>
+
+    <section>
+        <h2>Tips and tricks to crack these exams:</h2>
+        <ul>
+          <li>
+            <strong>Practice regularly:</strong> Regular practice helps you gain
+            confidence, improve weak areas, and develop problem-solving skills.
+            Make it a habit to practice daily. IIT Academy teachers are here to
+            help you and make sure you practice each chapter daily with regular
+            mock tests and quizzes.
+          </li>
+          <li>
+            <strong>Focus on Basics:</strong> Clear your basics in each subject
+            because they are the foundation for advanced concepts. Make sure you
+            understand the basics thoroughly. IIT Academy offers regular
+            doubt-clearing sessions and helps you strengthen your basics.
+          </li>
+          <li>
+            <strong>Time management:</strong> Learn to manage your time
+            effectively during the exams. Allocate time wisely for each question
+            and avoid spending too much time on one question. IIT Academy
+            provides regular mock tests to prepare you for the final exam.
+          </li>
+          <li>
+            <strong>Stay motivated and focused:</strong> Believe in yourself and
+            stay positive. Remember your goals, celebrate small victories, and
+            keep pushing forward.
+          </li>
+          <li>
+            <strong>Understand the exam pattern:</strong> Know the format,
+            marking scheme, and types of questions to avoid surprises on exam
+            day. IIT Academy will guide you through everything. You’ll learn all
+            about the exam strategies and patterns, making everything clear to
+            you.
+          </li>
+          <li>
+            <strong>Quality study materials:</strong> Use good study resources
+            like textbooks, online materials, and coaching classes to study the
+            right content. IIT Academy offers excellent chapter-wise study
+            materials created by professional and skilled teachers.
+          </li>
+          <li>
+            <strong>Seek help when needed:</strong> Don’t hesitate to ask
+            teachers, mentors, or friends for help if you face difficulties or
+            doubts. Our teachers are always here to help you.
+          </li>
+          <li>
+            <strong>Stay healthy and focused:</strong> Take care of your
+            physical and mental health by getting enough sleep, exercising, and
+            eating a balanced diet.
+          </li>
+        </ul>
+
+        <p>
+          By following these tips, you’ll be well-prepared to tackle JEE Mains
+          and Advanced with confidence! Stay tuned for more tips and guidance
+          from IIT Academy!
+        </p>
+    </section> `,
     link: "/blog/unlock-jee-secrets",
   },
 ];
--- smart-grader/src/Components/FullBlog/FullBlog.js
+++ smart-grader/src/Components/FullBlog/FullBlog.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+
+import { GoDash } from "react-icons/go";
 import DOMPurify from "dompurify";
 
 import posts from "../Blog/Posts";
+import "./FullBlog.css";
 
 const FullBlog = () => {
   const { postId } = useParams();
@@ -15,10 +18,27 @@ const FullBlog = () => {
   const sanitizedDescription = DOMPurify.sanitize(post.description);
 
   return (
-    <div>
-      <h2>{post.title}</h2>
-      <p>{post.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: sanitizedDescription }} />
+    <div className="container-fluid fullBlog-second">
+      <div className="row">
+        <div className="col-md-8 fullBlog">
+          <div className="fullBlogTitle">
+            <h3>{post.title}</h3>
+            <p>
+              <span> {post.date}</span> <GoDash /> | <span> BLOG</span>
+            </p>
+
+            <div className="img-container">
+              <img src={post.image} alt="blog-image" />
+            </div>
+          </div>
+
+          <div
+            className="fullBlogDescription"
+            dangerouslySetInnerHTML={{ __html: sanitizedDescription }}
+          />
+        </div>
+        <div className="col-md-4"></div>
+      </div>
     </div>
   );
 };

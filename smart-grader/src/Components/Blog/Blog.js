ec3a1eedcdd993d0ac9b758bf69de20decaf7c8f
nikitabaladha <nikitabaladha1996@gmail.com>
2024-10-09 00:38:46 +0530

    FullBlog.js page done with css

--- smart-grader/src/Components/Blog/Blog.js
+++ smart-grader/src/Components/Blog/Blog.js
@@ -7,19 +7,23 @@ import blogImage1 from "../../Images/blog-page-1.png";
 import { IoIosVideocam } from "react-icons/io";
 import { FaArrowRight } from "react-icons/fa6";
 
+// Function to truncate the HTML description to a maximum of 5 lines
 const truncateDescription = (html) => {
   const tempElement = document.createElement("div");
-  tempElement.innerHTML = html;
-  const textContent = tempElement.textContent || tempElement.innerText;
-  const lines = textContent.split("\n");
-  return lines.slice(0, 5).join("\n");
+  tempElement.innerHTML = html; // Set the inner HTML to the passed HTML string
+  const textContent = tempElement.textContent || tempElement.innerText; // Get text content
+  const lines = textContent.split("\n"); // Split text into lines
+  return lines.slice(0, 5).join("\n"); // Return the first 5 lines joined
 };
 
 const Blog = () => {
   return (
     <>
+      {/* Rendering the Navbar component */}
       <Navbar />
+
       <div className="container-fluid">
+        {/* Header section for the blog page */}
         <div className="container-fluid blog-page-first">
           <h2>Our Blog</h2>
           <p>
@@ -28,11 +32,14 @@ const Blog = () => {
           </p>
         </div>
 
+        {/* Main blog content section */}
         <div className="container-fluid blog-page-second">
           <div className="row">
             {posts.map((post, index) => (
+              // Mapping through the posts to create a blog card for each post
               <div className="col-md-6" key={index}>
                 <div className="blog-card">
+                  {/* Blog card header with tutorial label and date */}
                   <h6>
                     <span className="d-flex justify-content-between align-items-center mb-3">
                       <span>
@@ -41,21 +48,21 @@ const Blog = () => {
                         </span>
                         <span className="sub-title">Tutorial </span>
                       </span>
-                      <span className="date"> {post.date}</span>
+                      <span className="date">{post.date}</span>{" "}
                     </span>
                   </h6>
                   <h2>{post.title}</h2>
                   <p>{truncateDescription(post.description)}</p>
+
                   <div className="d-flex justify-content-between align-items-center card-footer">
                     <div className="d-flex">
                       <div className="img-container">
-                        <img src={blogImage1} alt="SmartGrader Team" />
+                        <img src={blogImage1} alt="SmartGrader Team" />{" "}
                       </div>
-
                       <p>SmartGrader Team</p>
                     </div>
                     <Link to={`/blog/${index}`}>
-                      Read more <FaArrowRight />
+                      Read more <FaArrowRight />{" "}
                     </Link>
                   </div>
                 </div>
@@ -68,4 +75,4 @@ const Blog = () => {
   );
 };
 
-export default Blog;
+export default Blog; // Exporting the Blog component
--- smart-grader/src/Components/FullBlog/FullBlog.js
+++ smart-grader/src/Components/FullBlog/FullBlog.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-
 import { GoDash } from "react-icons/go";
 import DOMPurify from "dompurify";
 
@@ -8,36 +7,62 @@ import posts from "../Blog/Posts";
 import "./FullBlog.css";
 
 const FullBlog = () => {
+  // Extracting the postId from the URL parameters
   const { postId } = useParams();
+
+  // Retrieving the specific post based on postId
   const post = posts[postId];
 
+  // If the post is not found, display a message
   if (!post) {
     return <h2>Post not found</h2>;
   }
 
+  // Sanitizing the post description to prevent XSS attacks
   const sanitizedDescription = DOMPurify.sanitize(post.description);
+
+  // Filtering out the current post to get recent posts
+  const recentPosts = posts.filter((_, index) => index !== parseInt(postId));
 
   return (
     <div className="container-fluid fullBlog-second">
       <div className="row">
+        {/* Main blog content section */}
         <div className="col-md-8 fullBlog">
           <div className="fullBlogTitle">
             <h3>{post.title}</h3>
             <p>
-              <span> {post.date}</span> <GoDash /> | <span> BLOG</span>
+              <span>{post.date}</span> <GoDash /> | <span>BLOG</span>
             </p>
-
+            {/* Displaying the main blog image */}
             <div className="img-container">
-              <img src={post.image} alt="blog-image" />
+              <img src={post.image} alt="blog" />
             </div>
           </div>
 
+          {/* Rendering the sanitized blog description */}
           <div
             className="fullBlogDescription"
             dangerouslySetInnerHTML={{ __html: sanitizedDescription }}
           />
         </div>
-        <div className="col-md-4"></div>
+
+        {/* Recent posts section */}
+        <div className="col-md-4 fullBlog-right">
+          <h3>Recent Posts</h3>
+          {recentPosts.length > 0 ? (
+            // Mapping over the recent posts to display them
+            recentPosts.map((recentPost, index) => (
+              <div key={index} className="recent-post">
+                <img src={recentPost.image} alt="recent-blog" />
+                <h4>{recentPost.title}</h4>
+              </div>
+            ))
+          ) : (
+            // Message displayed if no recent posts are available
+            <p>No recent posts available.</p>
+          )}
+        </div>
       </div>
     </div>
   );
